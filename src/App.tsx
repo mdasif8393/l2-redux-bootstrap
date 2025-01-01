73be9bfcf97b90f41c20faccc236de7081001da2
mdasif8393 <md.asif.8393@gmail.com>
2025-01-01 21:39:46 +0600

    23-11 Add Data with Action Payloads.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import "./App.css";
+import {
+  decrement,
+  decrementByPayload,
+  increment,
+  incrementByPayload,
+} from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
+import { RootState } from "./redux/store";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const { count } = useAppSelector((state: RootState) => state.counter);
+
   return (
     <>
       <h1>Content with Redux</h1>
-      <button>Increment</button>
-      <div>0</div>
-      <button>Decrement</button>
+      <button onClick={() => dispatch(incrementByPayload(5))}>
+        Increment By 5
+      </button>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <div>{count}</div>
+      <button onClick={() => dispatch(decrementByPayload(5))}>
+        Decrement By 5
+      </button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </>
   );
 }
--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -1,11 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState: number = 0;
+const initialState = {
+  count: 0,
+};
 
 const counterSlice = createSlice({
   name: "counter",
   initialState,
-  reducers: {},
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    incrementByPayload: (state, action) => {
+      state.count += action.payload;
+    },
+    decrementByPayload: (state, action) => {
+      state.count -= action.payload;
+    },
+  },
 });
 
+export const { increment, decrement, incrementByPayload, decrementByPayload } =
+  counterSlice.actions;
 export default counterSlice.reducer;
