74644e70256398160e17bf0398a1306caa002a97
mdasif8393 <md.asif.8393@gmail.com>
2025-01-04 00:17:49 +0600

    24-12 Handle form inputs and resolve warnings and errors.

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,4 +1,5 @@
 import TaskCard from "@/components/module/tasks/TaskCard";
+import AddTaskModal from "@/redux/features/task/AddTaskModal";
 import { selectTasks } from "@/redux/features/task/taskSlice";
 import { useAppSelector } from "@/redux/hook";
 
@@ -7,12 +8,13 @@ const Tasks = () => {
 
   return (
     <div className="mx-auto max-w-7xl px-5 mt-10">
-      <div>
+      <div className="flex justify-between items-center mb-2">
         <h1>Tasks</h1>
+        <AddTaskModal />
       </div>
       <div>
         {tasks.map((task) => (
-          <TaskCard task={task} />
+          <TaskCard task={task} key={task.id} />
         ))}
       </div>
     </div>
